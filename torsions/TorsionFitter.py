1b44d1ece3fc63fffdd168b8903a52eed9bffbf1
ChayaSt <chaya.stern@choderalab.org>
2015-05-12 15:49:16 -0400

    added TorsionScanSet class

--- torsions/TorsionFitter.py
+++ torsions/TorsionFitter.py
@@ -7,96 +7,120 @@ Created on Thu May  7 19:32:22 2015
 from collections import OrderedDict
 import pandas as pd
 from cclib.parser import Gaussian
+from cclib.parser.utils import convertor 
 import re
-import mdtraj as md
+from mdtraj import Trajectory
+import numpy as np
+import simtk.openmm as mm
+from simtk.unit import Quantity, kilojoules_per_mole, nanometer
 
-#def load_scan(filenames, top):
-#    """load Gaussian torsio scan log files from disk
-#    
-#    Parameters
-#    ---------
-#    filename: list of str
-#    top: str. Pass in the path to a RCB PDB file
-#    
-#    Return
-#    ------
-#    torsionscanset: torsion.TorsionScanSet
-#    """
-#    traj = md.load(top)
-#    for fi in filenames:
-#        filename = get_g09_filename(fi)
-#        fragment = get_frag_name(fi)
-#        torsion = get_torsion(fi)
-#        steps = get_steps(fi)
-#        log = Gaussian(fi)
-#        data = log.parse()
-        
-        
-    
-    
-def get_g09_filename(filename):
-    return filename.split('/')[-1]
 
-def get_frag_name(filename):
-    f = filename.split('/')[-1]
-    return f.split('.')[0]
+def read_scan_logfile(logfiles, topology):
+    """
+    parameters
+    ----------
+    logfiles: str of list of str
+                Name of Guassian 09 torsion scan log file
+    topology: mdtraj.Topology
 
-def get_torsion(filename):
-    f = open(filename, 'r')
-    for line in f:
-        if re.search('   Scan   ', line):
-            torsion = line.split()[2].split(',')
-            torsion[0] = torsion[0][-1]
-            torsion[-1] = torsion[-1][0]
-            for i in range(len(torsion)):
-                torsion[i] = (int(torsion[i]) - 1)
-    f.close()
-    return torsion
-    
-def get_steps(filename):
-    steps = []
-    index = (2,12,-1)
-    f = open(filename, 'r')
-    for line in f:
-        if re.search('Step', line):
-            try:
-                steps.append([line.rsplit()[j] for j in index])
-            except:
-                pass
-    f.close
-    return steps
-        
+    returns
+    -------
+    TorsionScanSet
+    """
 
-class TorsionScanSet(object):
+    positions = np.ndarray((0, topology.n_atoms, 3))
+    energies = np.ndarray(0)
+    torsions = np.ndarray((0, 4), dtype=int)
+    directions = np.ndarray((0, 1), dtype=int)
+    steps = np.ndarray((0, 3), dtype=int)
+
+    if type(logfiles) != list:
+        logfiles = [logfiles]
+
+    for file in logfiles:
+        print file
+        direction = np.ndarray((1,1))
+        torsion = np.ndarray((1,4), dtype=int)
+        step = []
+        index = (2, 12, -1)
+        f = file.split('/')[-1].split('.')
+        print f[2]
+        if f[2] == 'pos':
+            direction[0][0] = 1
+        else:
+            direction[0][0] = 0
+
+        fi = open(file, 'r')
+        for line in fi:
+
+            if re.search('   Scan   ', line):
+                t = line.split()[2].split(',')
+                t[0] = t[0][-1]
+                t[-1] = t[-1][0]
+                for i in range(len(t)):
+                    torsion[0][i] = (int(t[i]) - 1)
+            if re.search('Step', line):
+                try:
+                    step = np.array(([int(line.rsplit()[j]) for j in index]))
+                    step = step[np.newaxis,:]
+                    steps = np.append(steps, step, axis=0)
+                except:
+                    pass
+        fi.close()
+
+        log = Gaussian(file)
+        data = log.parse()
+        # convert angstroms to nanometers
+        positions = np.append(positions, data.atomcoords*0.1, axis=0)
+        energies = np.append(energies, convertor(data.scfenergies, "eV", "kJmol-1"), axis=0)
+        for i in range(len(data.scfenergies)):
+            torsions = np.append(torsions, torsion, axis=0)
+            directions = np.append(directions, direction, axis=0)
+
+    qm_energy = Quantity(value=energies, unit=kilojoules_per_mole)
+
+    return TorsionScanSet(positions, topology, torsions, directions, steps, qm_energy)
+
+
+class TorsionScanSet(Trajectory):
     """container object for torsion scan
     
     A TorsionScanSet should be constructed by loading Gaussian log files from the 
-    scan with a topoloyg - usually a PDB file
+    scan with a topology - usually a PDB file
     
     Attributes
     ----------
-    
-    positions: dict((str, str, str, str) : unit.Quantity(np.array[natoms, ndim], u.nm))
-    Dictionary mapping the 4 element tuple of mol_name, torsion, directions, and step
-    to the positions
-    torsion_set: pandas data frame. 
+    qm_energy: simtk.unit.Quantity((n_frames, 1), unit=kilojoule/mole)
+    mm_energy: simtk.unit.Quantity((n_frames, 1), unit=kilojoule/mole)
+    delta_energy: simtk.unit.Quantity((n_frames, 1), unit=kilojoule/mole)
+    torsion_index: {np.ndarray, shape(n_frames, 4)}
+    step: {np.ndarray, shape(n_frame, 3)}
+    direction: {np.ndarray, shape(n_frame, 1)}
     """
-    
-    def __init__(self):
+
+    def __init__(self, positions, topology, torsions, directions, steps, qm_energy):
         """Create new TorsionScanSet object"""
-        self._positions = OrderedDict()
-        self._torsion_set = pd.DataFrame()
-        
-        
-    def extract_opt(self):
-        """Returns TorsionScanSet object with only optimized geometries"""
-        
-    def compute_energy(self, frag,  param, platform=None):
-        """computes energy for a given structure with a given paramere set"""
-        
-        
-        
-    
-    
-    
-    
+        Trajectory.__init__(self, positions, topology)
+        self.qm_energy = qm_energy
+        self.mm_energy = Quantity()
+        self.delta_energy = Quantity()
+        self.torsion_index = torsions
+        self.direction = directions
+        self.steps = steps
+
+    def to_dataframe(self):
+        """ convert TorsionScanSet to pandas dataframe """
+
+        data = []
+        for i in range(self.n_frames):
+            if len(self.mm_energy) == self.n_frames and len(self.delta_energy) == self.n_frames:
+                data.append((self.torsion_index[i], self.direction[i], self.steps[i], self.qm_energy[i], self.mm_energy[i],
+                             self.delta_energy[i]))
+            else:
+                data.append((self.torsion_index[i], self.direction[i], self.steps[i], self.qm_energy[i], float('nan'), float('nan')))
+
+        torsion_set = pd.DataFrame(data, columns=[ "torsion", "scan_direction", "step_point_total", "QM_energy KJ/mol",
+                                                   "MM_energy KJ/mole", "delta KJ/mole"])
+
+        return torsion_set
+
