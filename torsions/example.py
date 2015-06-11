b7c0c1a03ff7d3319526e3aae5c07ad1eadb679f
ChayaSt <chaya.stern@choderalab.org>
2015-06-11 15:39:19 -0400

    updated example

--- torsions/example.py
+++ torsions/example.py
@@ -1,21 +1,12 @@
 # opemm imports
-import simtk.unit as u
 import simtk.openmm as mm
-import simtk.openmm.app as app
 
 # ParmEd imports
-from chemistry.charmm import  CharmmPsfFile
 from chemistry.charmm.parameters import CharmmParameterSet
 
 import TorsionScanSet, TorsionFitModel
 import glob
 from pymc import MCMC
-import matplotlib
-import matplotlib.pyplot as plt
-import time
-import cProfile
-import pstats
-import StringIO
 
 param = CharmmParameterSet('../charmm_ff/top_all36_cgenff.rtf', '../charmm_ff/par_all36_cgenff.prm')
 stream = '../structures/Pyrrol/pyrrol.str'
@@ -24,15 +15,12 @@ scan = glob.glob('../structures/Pyrrol/torsion-scan/*.log')
 pyrrol_scan = TorsionScanSet.read_scan_logfile(scan, structure)
 pyrrol_opt = pyrrol_scan.extract_geom_opt()
 #create pymc model
-platform = mm.Platform.getPlatformByName('Reference')
+platform = mm.Platform.getPlatformByName('CPU')
 model = TorsionFitModel.TorsionFitModel(param, stream, pyrrol_opt, platform=platform)
 #update param with missing parameters
 model.add_missing(param)
-sampler = MCMC(model.pymc_parameters)
+sampler = MCMC(model.pymc_parameters, db='pickle', dbname='pyrrol.database')
 
-cProfile.run('sampler.sample(iter=10, burn=0, thin=1)', 'statfile')
-stream = StringIO.StringIO()
-stats = pstats.Stats('statfile', stream=stream)
-stats.print_stats()
+sampler.sample(iter=10000)
+sampler.db.close()
 
-print(stream.getvalue())
