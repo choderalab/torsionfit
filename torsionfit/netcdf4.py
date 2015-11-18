3c293654bcfdb63f821eb7f536213ebd906e7514
ChayaSt <chaya.stern@choderalab.org>
2015-11-18 16:30:45 -0500

    added chains

--- torsionfit/netcdf4.py
+++ torsionfit/netcdf4.py
@@ -3,28 +3,28 @@ from pymc.database import base
 from pymc import six
 import pymc
 import numpy as np
+import os
 
 __all__ = ['Trace', 'Database']
+
 
 class Trace(base.Trace):
 
     """netCDF4 Trace class"""
 
     def _initialize(self, chain, length):
-        """Create a netCDF4 variable.
-        """
 
         if self._getfunc is None:
             self._getfunc = self.db.model._funs_to_tally[self.name]
 
     def tally(self, index, chain=-1):
         """ Add current value to chain
-        :param chain:
-        :return:
+        :param index: tall index
+        :param chain: int
         """
 
         value = self._getfunc()
-        self.db.ncfile.variables[self.name][index] = value
+        self.db.ncfile['Chain#%d' % chain].variables[self.name][index] = value
 
 
 class Database(base.Database):
@@ -42,39 +42,51 @@ class Database(base.Database):
         """
         self.__name__ = 'netcdf'
         self.dbname = dbname
+        self.mode = dbmode
         self.trace_names = []
         self._traces = {} # dictionary of trace objects
         self.__Trace__ = Trace
-        self.tally_index = 0
         # To do - tally index if appending to existing db
         self.chains = 0
 
-        # open netCDF4 file for writing
+        # check if database exists
+        db_exists = os.path.exists(self.dbname)
+
+        if db_exists and self.mode == 'w':
+            # overwrite
+            os.remove(self.dbname)
+
+        # open netCDF4 file
         self.ncfile = netcdf.Dataset(dbname, dbmode, version= 'NETCDF4')
 
         # Set global attributes
         setattr(self.ncfile, 'title', self.dbname)
+
+
 
     def _initialize(self, funs_to_tally, length=None):
 
         for name, fun in six.iteritems(funs_to_tally):
             if name not in self._traces:
                 self._traces[name] = self.__Trace__(name=name, getfunc=fun, db=self)
+                self._traces[name]._initialize(self.chains, length)
 
+        i = self.chains
+        self.ncfile.createGroup("Chain#%d" % i)
         # set dimensions
-        self.ncfile.createDimension('nsamples', 0) # unlimited number of iterations
+        self.ncfile['Chain#%d' % i].createDimension('nsamples', 0) # unlimited number of iterations
 
         for name, fun in six.iteritems(funs_to_tally):
             if not np.asarray(fun()).shape == ():
-                self.ncfile.createDimension(name, np.asarray(fun()).shape[0])
-                self.ncfile.createVariable(name, np.asarray(fun()).dtype.str, ('nsamples', name))
+                self.ncfile['Chain#%d' % i].createDimension(name, np.asarray(fun()).shape[0])
+                self.ncfile['Chain#%d' % i].createVariable(name, np.asarray(fun()).dtype.str, ('nsamples', name))
             else:
-                self.ncfile.createVariable(name, np.asarray(fun()).dtype.str, ('nsamples',))
-
+                self.ncfile['Chain#%d' % i].createVariable(name, np.asarray(fun()).dtype.str, ('nsamples',))
 
         self.trace_names.append(list(funs_to_tally.keys()))
 
         self.chains += 1
+        self.tally_index = 0
 
     def connect_model(self, model):
         """Link the Database to the Model instance.
@@ -139,6 +151,21 @@ Error:
                 self.trace_names[chain].remove(name)
 
         self.tally_index += 1
+
+def load(self, dbname, dbmode='a'):
+    """ Load an existing netcdf database
+
+    :param dbname: name of netcdf file to open
+    :param dbmode: 'a': append, 'r': read-only
+    :return: database
+    """
+    if dbmode == 'w':
+        raise AttributeError("dbmode='w' not allowed for load")
+
+    db = Database(dbname, dbmode=dbmode)
+
+    return db
+
 
 
 
