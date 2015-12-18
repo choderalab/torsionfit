583efa70f5842879b932e663708a3e7c6038bf5e
ChayaSt <chaya.stern@choderalab.org>
2015-12-18 14:36:51 -0500

    added callgraph

--- examples/example.py
+++ examples/example.py
@@ -13,23 +13,23 @@ from pymc import MCMC
 import torsionfit.netcdf4 as db
 #from memory_profiler import profile
 
-param = CharmmParameterSet('../charmm_ff/top_all36_cgenff.rtf', '../charmm_ff/par_all36_cgenff.prm')
-stream = ['pyrrole/pyrrol.str', 'pyrrole-2/methyl-pyrrol.str']
-structure = ['pyrrole/pyrrol.psf', 'pyrrole-2/methyl-pyrrol.psf']
-scan = glob.glob('pyrrole/torsion-scan/*.log')
+param = CharmmParameterSet('data/charmm_ff/top_all36_cgenff.rtf', 'data/charmm_ff/par_all36_cgenff.prm')
+stream = ['data/pyrrole/pyrrol.str', 'data/pyrrole-2/methyl-pyrrol.str']
+structure = ['data/pyrrole/pyrrol.psf', 'data/pyrrole-2/methyl-pyrrol.psf']
+scan = glob.glob('data/pyrrole/torsion-scan/*.log')
 pyrrol_scan = TorsionScanSet.read_scan_logfile(scan, structure[0])
 pyrrol_opt = pyrrol_scan.extract_geom_opt()
-pyrrol_2_scan = TorsionScanSet.read_scan_logfile(glob.glob('pyrrole-2/torsion-scan/*.log'), structure[1])
+pyrrol_2_scan = TorsionScanSet.read_scan_logfile(glob.glob('data/pyrrole-2/torsion-scan/*.log'), structure[1])
 pyrrol_2_opt = pyrrol_2_scan.extract_geom_opt()
 frags = [pyrrol_opt, pyrrol_2_opt]
 #create pymc model
 platform = mm.Platform.getPlatformByName('Reference')
-model = TorsionFitModel.TorsionFitModel(param, stream, frag, platform=platform)
+model = TorsionFitModel.TorsionFitModel(param, stream, frags, platform=platform)
 #update param with missing parameters
 model.add_missing(param)
 sampler = MCMC(model.pymc_parameters, db=db, dbname='/Users/sternc1/src/ChayaSt/Torsions/examples/test.nc', verbose=5)
 
-sampler.sample(iter=10)
+sampler.sample(iter=1)
 
 #cProfile.run('sampler.sample(iter=50)', 'statfile')
 #sampler.db.close()
