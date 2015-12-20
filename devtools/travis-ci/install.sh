83f4893be12fc5551e602b4ae1b846da56c965b9
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-12-19 19:54:36 -0500

    Tweaks.

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -10,10 +10,6 @@ fi
 
 MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget http://repo.continuum.io/miniconda/$MINICONDA
-if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
-    echo "Miniconda MD5 mismatch"
-    exit 1
-fi
 bash $MINICONDA -b -p miniconda
 
 export PATH=$HOME/miniconda/bin:$PATH
