8b1e9ca469d0770a580e7fdfca1489fdd3250c90
Chaya Stern <chaya.stern@choderalab.org>
2016-04-19 11:01:52 -0400

    added tests for sqlite_plus backend

--- torsionfit/tests/test_backends.py
+++ torsionfit/tests/test_backends.py
@@ -9,8 +9,9 @@ from pymc.examples import disaster_model
 from pymc import MCMC
 import pymc
 import pymc.database
-import torsionfit.netcdf4 as db
-from pymc.tests.test_database import TestPickle
+import torsionfit.netcdf4 as netcdf4
+import torsionfit.sqlite_plus as sqlite_plus
+from pymc.tests.test_database import TestPickle, TestSqlite
 import numpy as np
 import nose
 import warnings
@@ -39,12 +40,12 @@ class TestNetcdf4(TestPickle):
         #if 'netcdf4' not in dir(pymc.database):
          #   raise nose.SkipTest
 
-        self.S = pymc.MCMC(disaster_model, db=db, dbname=os.path.join(testdir, 'Disaster.netcdf4'), dbmode='w')
+        self.S = pymc.MCMC(disaster_model, db=netcdf4, dbname=os.path.join(testdir, 'Disaster.netcdf4'), dbmode='w')
         self.S.use_step_method(pymc.Metropolis, self.S.early_mean, tally=True)
         self.S.use_step_method(pymc.Metropolis, self.S.late_mean, tally=True)
 
     def load(self):
-        return db.load(os.path.join(testdir, 'Disaster.netcdf4'))
+        return netcdf4.load(os.path.join(testdir, 'Disaster.netcdf4'))
 
 #    def test_data(self):
 #        self.db = self.load()
@@ -53,4 +54,28 @@ class TestNetcdf4(TestPickle):
 #        del self.db
 
 
+class TestSqlitePlus(TestSqlite):
 
+    @classmethod
+    def setUpClass(self):
+
+        self.S = pymc.MCMC(disaster_model, db=sqlite_plus, dbname=os.path.join(testdir, 'Disaster.sqlite'), dbmode='w')
+
+    def load(self):
+        return sqlite_plus.load(os.path.join(testdir, 'Disaster.sqlite'))
+
+    def test_yrestore_state(self):
+
+        original_filters = warnings.filters[:]
+        warnings.simplefilter("ignore")
+
+        try:
+            db = self.load()
+            S = pymc.MCMC(disaster_model, db=db)
+            S.use_step_method(pymc.Metropolis, S.early_mean)
+            S.use_step_method(pymc.Metropolis, S.late_mean)
+            S.sample(10, progress_bar=0)
+            sm = S.step_methods.pop()
+            assert_equal(sm.accepted + sm.rejected, 75)
+        finally:
+            warnings.filters = original_filters
