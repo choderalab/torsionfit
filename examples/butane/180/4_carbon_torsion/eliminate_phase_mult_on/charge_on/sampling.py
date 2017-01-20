9b3372096ee3f4a76938f673931152a7da083aa0
ChayaSt <chaya.stern@choderalab.org>
2017-01-19 19:25:41 -0500

    sampling scripts

---
+++ examples/butane/180/4_carbon_torsion/eliminate_phase_mult_on/charge_on/sampling.py
@@ -0,0 +1,20 @@
+import simtk.openmm as mm
+from torsionfit import TorsionScanSet as ScanSet
+import torsionfit.TorsionFitModel as Model
+from torsionfit import sqlite_plus
+from pymc import MCMC
+from parmed.charmm import CharmmParameterSet
+
+param = CharmmParameterSet('../../../../../data/charm_ff/top_all36_cgenff.rtf',
+                           '../../../../../data/charmm_ff/par_all36_cgenff.prm')
+structure = '../../../../structure/butane.psf'
+scan = '../../../../torsion_scans/DFT_b3lyp/butane_scan_b3lyp_180.log'
+butane_scan = ScanSet.parse_psi4(scan, structure)
+
+platform = mm.Platform.getPlatformByName('Reference')
+
+model = Model.TorsionFitModelEliminatePhase(param, butane_scan, platform=platform,
+                                            param_to_opt=[('CG331', 'CG321', 'CG321', 'CG331')])
+
+sampler = MCMC(model.pymc_parameters, db=sqlite_plus, dbname='butane_180_mult_on_charge_on.db', verbose=5)
+sampler.sample(1000000)
