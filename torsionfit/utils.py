7a6b02f6390d73ce709601395d06822229c085eb
ChayaSt <chaya.stern@choderalab.org>
2017-02-07 16:48:37 -0500

    added util function to get 0,1 traces for multiplicity bitstring

--- torsionfit/utils.py
+++ torsionfit/utils.py
@@ -14,3 +14,25 @@ def RMSE(scanSet, db):
         rmse = np.linalg.norm(energy - scanSet.qm_energy)/np.sqrt(N)
         errors[i] = rmse
     return errors
+
+
+def get_multiplicity_trace(key, db):
+    """
+    This function takes a bitstring key and data base and returns a dictionary of [0,1] traces for each multiplicity
+    term. 0 when that multiplicity is off and 1 when it's on
+    :param key: str (A_B_C_D_multiplicity_bitstring)
+    :param db: pymc sqlite_plus database
+    :return: dictionary of multiplicity mapped to lists of 0 and 1
+
+    """
+    multiplicities = (1, 2, 3, 4, 6)
+    multiplicity_trace = {}
+    for m in multiplicities:
+        multiplicity_trace[str(m)] = []
+        for i in db.trace(key)[:]:
+            if 2**(m-1) & int(i):
+                multiplicity_trace[str(m)].append(1)
+            else:
+                multiplicity_trace[str(m)].append(0)
+    return multiplicity_trace
+
