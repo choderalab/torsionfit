240872b7b7b1d41566ecfe27746ec558e6b54554
ChayaSt <chaya.stern@choderalab.org>
2017-02-28 11:31:56 -0500

    longer simulation

--- examples/butane/hydration_energy/generate_simulation_data.py
+++ examples/butane/hydration_energy/generate_simulation_data.py
@@ -10,6 +10,6 @@ params = CharmmParameterSet('../../data/charmm_ff/top_all36_cgenff.rtf', '../../
                            '../../data/charmm_ff/toppar_water_ions.str')
 
 db = energy.create_openmm_system('butane', '../structure', params)
-db_solv = energy.generate_simulation_data(db, params)
+db_solv = energy.generate_simulation_data(db, params, n_steps=4000, n_iter=500)
 
 pickle.dump(db_solv, open('solvated.pickle', 'w'))
