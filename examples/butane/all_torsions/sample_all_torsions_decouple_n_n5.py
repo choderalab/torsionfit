1cc25039147105233145037c37793be55ade1473
Chaya Stern <chayas@mskcc-ln1.local>
2017-03-04 21:11:54 -0500

    final script

--- examples/butane/all_torsions/sample_all_torsions_decouple_n_n5.py
+++ examples/butane/all_torsions/sample_all_torsions_decouple_n_n5.py
@@ -4,17 +4,14 @@ import torsionfit.TorsionFitModel as Model
 from torsionfit import sqlite_plus
 from pymc import MCMC
 from parmed.charmm import CharmmParameterSet
-
 param_to_opt=[('CG331', 'CG321', 'CG321', 'CG331'),
-              ('HGA3', 'CG331', 'CG321', 'HGA2'),
-              ('HGA2', 'CG321', 'CG331', 'HGA3'),
-              ('HGA3', 'CG331', 'CG321', 'CG321'),
-              ('CG321', 'CG321', 'CG331', 'HGA3'),
-              ('HGA2', 'CG321', 'CG321', 'HGA2'),
-              ('CG331', 'CG321', 'CG321', 'HGA2')]
-param = CharmmParameterSet('../data/charmm_ff/top_all36_cgenff.rtf', '../data/charmm_ff/par_all36_cgenff.prm')
-structure = 'structure/butane.psf'
-scan = 'MP2_torsion_scan/'
+	      ('HGA3', 'CG331', 'CG321', 'HGA2'),
+	      ('HGA3', 'CG331', 'CG321', 'CG321'),
+	      ('HGA2', 'CG321', 'CG321', 'HGA2'),
+	      ('CG331', 'CG321', 'CG321', 'HGA2')]
+param = CharmmParameterSet('../../data/charmm_ff/top_all36_cgenff.rtf', '../../data/charmm_ff/par_all36_cgenff.prm')
+structure = '../structure/butane.psf'
+scan = '../MP2_torsion_scan/'
 # Print initial guess
 print param.dihedral_types[param_to_opt[0]]
 
@@ -33,7 +30,7 @@ param.dihedral_types[('HGA3', 'CG331', 'CG321', 'CG321')][0].phi_k=0
 param.dihedral_types[('CG321', 'CG321', 'CG331', 'HGA3')][0].phi_k=0
 param.dihedral_types[('HGA2', 'CG321', 'CG321', 'HGA2')][0].phi_k=0
 param.dihedral_types[('CG331', 'CG321', 'CG321', 'HGA2')][0].phi_k=0
-
+param.dihedral_types[('HGA2', 'CG321', 'CG321', 'CG331')][0].phi_k=0
 # Create butane scan with torsions off
 optimized_0 = butane_scan.remove_nonoptimized()
 optimized_0.compute_energy(param)
@@ -41,5 +38,5 @@ optimized_0.compute_energy(param)
 platform = mm.Platform.getPlatformByName('Reference')
 model = Model.TorsionFitModelEliminatePhase(param, optimized_0, platform=platform,
                                             param_to_opt=param_to_opt, decouple_n=True, sample_n5=True)
-sampler = MCMC(model.pymc_parameters, db=sqlite_plus, dbname='butane_all_n5_decouple_n.db', verbose=5)
-sampler.sample(100000)
+sampler = MCMC(model.pymc_parameters, db=sqlite_plus, dbname='butane_all_n5_decouple_n_10000.db', verbose=5)
+sampler.sample(10000)
