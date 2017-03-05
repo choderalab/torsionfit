5b203bb0deaebd3dd06368e01977e404d9bbefcd
Chaya Stern <chayas@mskcc-ln1.local>
2017-03-04 22:11:33 -0500

    generate solvated sim data

--- examples/data/pyrrole/hydration_energy/generate_simulation_data.py
+++ examples/data/pyrrole/hydration_energy/generate_simulation_data.py
@@ -9,15 +9,15 @@ except:
 params = CharmmParameterSet('../../charmm_ff/top_all36_cgenff.rtf', '../../charmm_ff/par_all36_cgenff.prm',
                            '../../charmm_ff/toppar_water_ions.str', '../pyrrol.str')
 
-db = energy.create_openmm_system('butane', '../structure', params)
-db_solv = energy.generate_simulation_data(db, params, n_steps=4000, n_iter=500)
+db = energy.create_openmm_system('pyrrole', '../structure', params)
+db_solv = energy.generate_simulation_data(db, params, n_steps=3000, n_iter=400)
 
 # remove system and context because pickle is giving me problems pickling them
 
 db_new = {}
 for key in db_solv['solvated'].keys():
     if key == 'system' or key == 'context':
-        db_new[key] = db_solv[key]
+        db_new[key] = db_solv['solvated'][key]
 
 pickle.dump(db_new, open('solvated.pickle', 'w'))
 pickle.dump(db_solv, open('solvated_test.pickle', 'w'))
