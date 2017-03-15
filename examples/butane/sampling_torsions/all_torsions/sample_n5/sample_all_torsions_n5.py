f9111ddc46a2a2a9f2aea75f3eb079cd0692bd6d
ChayaSt <chaya.stern@choderalab.org>
2017-03-14 21:39:39 -0400

    sampling all torsions

--- examples/butane/sampling_torsions/all_torsions/sample_n5/sample_all_torsions_n5.py
+++ examples/butane/sampling_torsions/all_torsions/sample_n5/sample_all_torsions_n5.py
@@ -13,7 +13,7 @@ param_to_opt=[('CG331', 'CG321', 'CG321', 'CG331'),
 param = CharmmParameterSet('../../../../data/charmm_ff/top_all36_cgenff.rtf',
 						   '../../../../data/charmm_ff/par_all36_cgenff.prm')
 structure = '../../../structure/butane.psf'
-scan = '../../../torsion_scan/MP2_torsion_scan/'
+scan = '../../../torsion_scans/MP2_torsion_scan/'
 # Print initial guess
 print param.dihedral_types[param_to_opt[0]]
 
