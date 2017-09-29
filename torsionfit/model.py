c241d612fdae85106d17d4a52c430cabc6917628
ChayaSt <chaya.stern@choderalab.org>
2017-09-29 09:10:56 -0400

    updated documentation

--- torsionfit/model.py
+++ torsionfit/model.py
@@ -2,30 +2,27 @@ __author__ = 'Chaya D. Stern'
 
 import pymc
 import numpy as np
-from simtk.unit import kilojoules_per_mole
 import torsionfit.database.qmdatabase as TorsionScan
-import torsionfit.parameters as par
-import warnings
 from torsionfit.utils import logger
 from collections import OrderedDict
 import itertools
 
 
 class TorsionFitModel(object):
-    """pymc model
-
-    This model only allows a phase angle of 0 but allows force constants to flip signs. If the sign is negative, the
-    phase angle will be 180.
+    """pymc model for sampling torsion parameters.
+    This model provides the option to use reversible jump to sample over multiplicity terms
 
     Attributes:
     ----------
     pymc_parameters: dict() of pymc parameters
+    fags: list of QMDatabasees for fragments to fit torsions to
+    rj: bool. If True, model uses reversible jump to sample over multiplicity terms. If false, all terms are sampled.
     parameters_to_optimize: list of tuples (dihedrals to optimize)
-    fags: list of TorsionScanSet for fragments
-    platform: OpenMM platform to use for potential energy calculations
+    models: list of models to sample over.
+    inner_sum: list of precalculated inner sum. This is also the gradient.
 
     """
-    def __init__(self, param, frags, stream=None,  platform=None, param_to_opt=None, rj=False, init_random=True, tau='mult'):
+    def __init__(self, param, frags, stream=None,  param_to_opt=None, rj=False, init_random=True, tau='mult'):
         """
 
         Parameters
@@ -33,18 +30,19 @@ class TorsionFitModel(object):
         param : Parmed CharmmParameterSet
         frags : list of torsionfit.QMDataBase
         stream : str
-            Path to CHARMM stream file. Default None.
-        platform : openmm.Platform
-            Default None.
+            Path to CHARMM stream file. Default None. If None, param_to_opt list must be given. When a stream file is
+            specified, param_to_opt is generated if the penalty of the parameters are greater than a threshold.
         param_to_opt : list of tuples of torsions.
             Default None.
         rj : bool
             If True, will use reversible jump to sample Fourier terms. If False, will sample all Ks. Default False
         init_random: bool
             Randomize starting condition. Default is True. If false, will resort to whatever value is in the parameter set.
+            Default True
         tau: string.
-            options are mult or single. When mult, every element in K_m will have its own tau, when single, each K_m will
-             have one tau.
+            options are 'mult' or 'single'. When 'mult', every element in K_m will have its own 'tau', when 'single',
+            each K_m will have one tau.
+            Default 'mult'
 
         Returns
         -------
@@ -57,7 +55,6 @@ class TorsionFitModel(object):
 
         self.pymc_parameters = dict()
         self.frags = frags
-        self.platform = platform
         self.rj = rj
         if param_to_opt:
             self.parameters_to_optimize = param_to_opt
@@ -77,11 +74,13 @@ class TorsionFitModel(object):
         elif tau == 'single':
             value = np.log(0.01)
         else:
-            raise Warning('Only mult and single are allowed options for tau')
+            raise Exception("Only 'mult' and 'single' are allowed options for tau")
 
         for p in self.parameters_to_optimize:
             torsion_name = p[0] + '_' + p[1] + '_' + p[2] + '_' + p[3]
 
+            # lower and upper for this distribution are based on empirical data that below this amount the prior is too
+            # biased and above the moves are usually rejected.
             self.pymc_parameters['log_sigma_k_{}'.format(torsion_name)] = pymc.Uniform('log_sigma_k_{}'.format(torsion_name),
                                                                                        lower=-4.6052, upper=3.453,
                                                                                        value=value)
@@ -121,16 +120,16 @@ class TorsionFitModel(object):
 
         # Precalculate phis
         n = np.array([1., 2., 3., 4., 5., 6.])
-        self.bitmasks = []
-        for i in itertools.product((0,1), repeat=6):
-            self.bitmasks.append(i)
+        self.models = []
+        for i in itertools.product((0, 1), repeat=6):
+            self.models.append(i)
 
-        cosines = []
+        inner_sum = []
         for i, frag in enumerate(frags):
-            cosines.append(OrderedDict())
+            inner_sum.append(OrderedDict())
             for t in frag.phis:
-                cosines[i][t] = (1 + np.cos(frag.phis[t][:, np.newaxis]*n[:, np.newaxis])).sum(-1)
-        self.cosines = cosines
+                inner_sum[i][t] = (1 + np.cos(frag.phis[t][:, np.newaxis]*n[:, np.newaxis])).sum(-1)
+        self.inner_sum = inner_sum
 
         @pymc.deterministic
         def torsion_energy(pymc_parameters=self.pymc_parameters):
@@ -138,13 +137,13 @@ class TorsionFitModel(object):
 
             for i, mol in enumerate(self.frags):
                 Fourier_sum = np.zeros((mol.n_frames))
-                for t in cosines[i]:
+                for t in inner_sum[i]:
                     name = t[0] + '_' + t[1] + '_' + t[2] + '_' + t[3]
                     if self.rj:
-                        K = pymc_parameters['{}_K'.format(name)] * self.bitmasks[pymc_parameters['{}_multiplicity_bitstring'.format(name)]]
+                        K = pymc_parameters['{}_K'.format(name)] * self.models[pymc_parameters['{}_multiplicity_bitstring'.format(name)]]
                     else:
                         K = pymc_parameters['{}_K'.format(name)]
-                    Fourier_sum += (K*cosines[i][t]).sum(1)
+                    Fourier_sum += (K*inner_sum[i][t]).sum(1)
                 Fourier_sum_rel = Fourier_sum - min(Fourier_sum)
                 Fourier_sum_rel += pymc_parameters['{}_offset'.format(mol.topology._residues[0])]
                 mm = np.append(mm, Fourier_sum)
