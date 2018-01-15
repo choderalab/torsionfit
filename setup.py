c6fb8d9739ba20d33d70a510ac57fc07a45901b7
ChayaSt <chaya.stern@choderalab.org>
2018-01-15 17:30:06 -0500

    added qmscan package data yaml file

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     platforms=['Linux', 'Mac OS-X', 'Unix', 'Windows'],
     classifiers=CLASSIFIERS.splitlines(),
     packages=['torsionfit', 'torsionfit.tests', 'torsionfit.qmscan', 'torsionfit.backends', 'torsionfit.database'],
-    package_data={'': package_files('torsionfit/tests/reference')},
+    package_data={'': package_files('torsionfit/tests/reference'), '': package_files('torsionfit/qmscan')},
     zip_safe=False,
     install_requires=[
         'numpy',
