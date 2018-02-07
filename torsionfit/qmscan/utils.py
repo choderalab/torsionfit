1254f755dcd039f6526a19a55b8ed1bff6f75c1e
ChayaSt <chaya.stern@choderalab.org>
2018-02-07 16:36:37 -0500

    added utils.py to qmscan package

---
+++ torsionfit/qmscan/utils.py
@@ -0,0 +1,79 @@
+from openeye import oechem
+import os
+
+
+def write_oedatabase(moldb, ofs, mlist, size):
+    """
+    This function writes out a new oedatabase from an existing database
+
+    Parameters
+    ----------
+    moldb: OpenEye database object
+    ofs: output stream
+    mlist: list of indices of molecules in moldb for new database
+    size: size of new database
+
+    """
+    for molidx in mlist[:size:]:
+        moldb.WriteMolecule(ofs, molidx)
+
+
+def to_smi(smiles, path, base):
+    """
+    This function writes out an .smi file for a list of SMILES
+    Parameters
+    ----------
+    smiles: list of SMILES
+    path: str
+        path to output file
+    base: str
+        base name for output file
+
+    """
+    fname = os.path.join(path, base + '.smi')
+    outf = open(fname, 'w')
+    smiles_list = map(lambda x: x+"\n", list(smiles))
+    outf.writelines(smiles_list)
+    outf.close()
+
+
+def create_oedatabase_idxfile(ifname):
+    """
+    This function creates an index file associated with a given molecule filename. It write out the file in the same
+    directory the parent molecule file is and adds an .idx extension to parent molecule filename.
+
+    From OpenEye's documentation:
+    The index file of a molecule dtabase stores file position offsets of the molecules in the file. Generating an index
+    file can be expensive, but it can be created only once and then it can speed up the handling of large molecule files
+    significantly
+
+    Parameters
+    ----------
+    ifname: str
+        absolute path to molecule file
+    """
+    idx_fname = oechem.OEGetMolDatabaseIdxFileName(ifname)
+
+    if os.path.exists(idx_fname):
+        oechem.OEThrow.Warning("{} index file already exists".format(idx_fname))
+    elif not oechem.OECreateMolDatabaseIdx(ifname):
+        oechem.OEThrow.Warning("Unable to create {} molecule index file".format(idx_fname))
+
+
+def new_output_stream(outname):
+    """
+    This function creates a new oechem.oemolostream.
+    Parameters
+    ----------
+    outname: str
+        name of outputfile.
+
+    Returns
+    -------
+    ofs: oechem.oemolostream
+
+    """
+    ofs = oechem.oemolostream()
+    if not ofs.open(outname):
+        oechem.OEThrow.Fatal("Unable to open {} for writing".format(outname))
+    return ofs
